1bba3d670305e1a4d1e73390dd048826d15a5b42
Ronilson Santos <labin10@labin10-Digitron.(none)>
2012-11-13 20:36:04 -0200

    criando minha primeira aplicação RAILS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fasol::Application.config.secret_token = 'd242fb6081323bd719fcd6358b43a51847354e990e9fc3afe9dac302fb35709c95d3b466ebb7c76a3e28a96350a649a23949d5e2c96254468684015142605f85'
